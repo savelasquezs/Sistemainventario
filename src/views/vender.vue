241a5d0ae0d976025ef472bc3bfc1dd6b702b5ef
Miguel Correa <sernamiguel441@gmail.com>
2023-04-01 17:42:54 -0500

    feat: :art:

--- src/views/vender.vue
+++ src/views/vender.vue
@@ -1,29 +1,33 @@
 <template>
-    <div>28 Octubre 2021 | 17:30</div>
-    <div class="container">
-        <ul class="compras-ul">
-            <li>Compras</li>
-        </ul>
+  <div>28 Octubre 2021 | 17:30</div>
+  <div class="container">
+    <ul class="compras-ul">
+      <li>Compras</li>
+    </ul>
+  </div>
 
-        <ul>
-            <li class="productos">
-                <div class="vender-producto">
-                    <img src="../assets/img/toxiFondoBlanco.jpeg" alt="">
-                </div>
-                <div class="product">
-                    <div class="nombre-producto">
-                        <h5>Gorra Flex-Fit</h5>
-                        <h6>$4.29</h6>
-                    </div>
-                    <div class="remove-add">
-                        <Icon icon="ic:baseline-remove-circle" width="30" height="30" />
-                        <h4>1</h4>
-                        <Icon icon="material-symbols:add-circle-rounded" width="30" height="30" />
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </div>
+  <ul>
+    <li class="productos">
+      <div class="vender-producto">
+        <img src="../assets/img/toxiFondoBlanco.jpeg" alt="" />
+      </div>
+      <div class="product">
+        <div class="nombre-producto">
+          <h5>Gorra Flex-Fit</h5>
+          <h6>$4.29</h6>
+        </div>
+        <div class="remove-add">
+          <Icon icon="ic:baseline-remove-circle" width="30" height="30" />
+          <h4>1</h4>
+          <Icon
+            icon="material-symbols:add-circle-rounded"
+            width="30"
+            height="30"
+          />
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -45,27 +49,37 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.compras-ul{
-    padding: 20px;
-    margin: 10px;
-    cursor: pointer;
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+  gap: 10px;
+
+  width: 836px;
+
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
 }
 
 .productos {
   display: flex;
 }
 
-.vender-producto{
-    img {
-        width: 140px;
-    }
+.vender-producto {
+  img {
+    width: 140px;
+  }
 }
 
 .product {
-    display: grid;
-    align-items: center;
-    flex-direction: column;
-    justify-content: space-between;
+  display: grid;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .remove-add {
@@ -76,6 +90,4 @@ export default {
   padding: 0px;
   gap: 30px;
 }
-
-
 </style>
